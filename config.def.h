17707b374e09ed68a88a7dc42b0823f90540fbd7
Daniel Langesten <daniel.langest@gmail.com>
2015-07-20 20:17:47 +0200

    added commands for new tilings

--- config.def.h
+++ config.def.h
@@ -103,9 +103,11 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[3]} }, /* bstack */
+	{ MODKEY,                       XK_y,      setlayout,      {.v = &layouts[0]} }, /* original */
+	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[4]} }, /* bstack horiz */
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} }, /* floating */
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} }, /* monocle */
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
